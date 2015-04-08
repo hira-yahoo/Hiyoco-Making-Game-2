f7884400aab9d0e4a6243e3780aa0b6b850e57b0
hira-yahoo <misocasu@yahoo.co.jp>
2015-04-08 14:42:47 +0900

    display elapsed time.

---
+++ Assets/TimeText.js
@@ -0,0 +1,22 @@
+﻿#pragma strict
+
+static var time : float;
+
+function Start () {
+
+	time = 0;
+}
+
+function Update () {
+
+	if ( GoalArea.goal == false )
+	{
+		time += Time.deltaTime;
+	}
+	
+	var now : int = time;
+	var text = GetComponent( UnityEngine.UI.Text );
+	text.text = "<Color=red>TIME: " + now + "</Color>";
+		
+
+}
